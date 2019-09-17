72ab0fb5e0ddb744c888dab2e31520cd84464928
Gerald Tee <93geraldtee@gmail.com>
2019-09-17 17:47:45 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,19 +3,19 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  return num1.to_i - num2.to_i
 end
 
 def division(num1, num2)
-
+  return num1.to_i / num2.to_i
 end
 
 def multiplication(num1, num2)
-
+  return num1.to_i * num2.to_i
 end
 
 def modulo(num1, num2)
-
+  return 
 end
 
 def square_root(num)
